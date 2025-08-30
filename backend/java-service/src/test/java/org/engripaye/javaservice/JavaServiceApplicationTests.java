b17e973d566055c4e668666a8b28b82cd3995b84
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-30 03:00:07 +0100

    commit init by ENGR IPAYE

---
+++ backend/java-service/src/test/java/org/engripaye/javaservice/JavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engripaye.javaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
